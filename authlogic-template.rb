c51410b0c835fa1fa6e013399a74e830550639f5
Hector Sansores <hector.sansores@virtus.com.mx>
2009-04-15 21:40:30 -0500

    deleted todo comment

--- authlogic-template.rb
+++ authlogic-template.rb
@@ -105,8 +105,6 @@ class UsersController < ApplicationController
 end
 FILE
 
-# TODO falta agregar las vistas
-
 file "app/controllers/application_controller.rb", <<-FILE
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
